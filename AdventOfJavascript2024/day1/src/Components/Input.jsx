ffc6e5d56376ccd551d036e42b4931e097d33752
Tom MURPHY <158026986+BomberQLF@users.noreply.github.com>
2024-12-03 20:17:24 +0100

    Delete AdventOfJavascript2024 directory

--- AdventOfJavascript2024/day1/src/Components/Input.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const Input = ({ type, name, className }) => {
-  return <input type={type} name={name} className={className} />;
-};
-export default Input;
--- AdventOfJavascript2024/day1/src/Components/Title.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Title = ({className}) => {
-    return (
-        <div className={className}>Day One</div>
-    )
-}
-
-export default Title;
